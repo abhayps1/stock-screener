13af374ce0746e55251719dba784ff348cfa16d1
abhayps1 <abhaypratapsingh1809@gmail.com>
2025-06-29 21:21:09 +0530

    IPO page added to the navbar

--- bse-corporate-announcement-frontend/src/app/app-routing.module.ts
+++ bse-corporate-announcement-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IpoListComponent } from './ipo-list/ipo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ipo', component: IpoListComponent },
+  // { path: '', redirectTo: '/ipo', pathMatch: 'full' } // Optional: set IPO as default
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
